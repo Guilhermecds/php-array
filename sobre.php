3077f7497008f95f72570f5b4e24d1ebcbecd000
Guilhermecds <55598098+Guilhermecds@users.noreply.github.com>
2022-08-19 21:55:21 -0300

    Add files via upload

---
+++ sobre.php
@@ -0,0 +1,18 @@
+<?php include "header.php"; ?>
+<main>
+    <h1> Sobre a IMDB</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+    Iste nobis ipsa suscipit magnam voluptate consequuntur harum alias asperiores, 
+    repudiandae aut sapiente placeat ipsum, fugit perspiciatis voluptates quis. Officiis,
+    aperiam rerum?</p>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+    Iste nobis ipsa suscipit magnam voluptate consequuntur harum alias asperiores, 
+    repudiandae aut sapiente placeat ipsum, fugit perspiciatis voluptates quis. Officiis,
+    aperiam rerum?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+    Iste nobis ipsa suscipit magnam voluptate consequuntur harum alias asperiores, 
+    repudiandae aut sapiente placeat ipsum, fugit perspiciatis voluptates quis. Officiis,
+    aperiam rerum?</p>
+</main>
+<?php include "footer.php"; ?>
